943fbf42d4cb3245e1ac25683c176e4ff7e66ec8
bogdyname <bogdyname@gmail.com>
2020-08-26 18:15:42 -0500

    voic recognition - it's working on Linux (only eng words)

---
+++ Python/speechtest.py
@@ -0,0 +1,12 @@
+import speech_recognition as sp
+
+r = sp.Recognizer()
+
+with sp.Microphone() as source:
+    print("Speak Anything :")
+    audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print("You said : {}".format(text))
+    except:
+        print("Sorry could not recognize what you said")
