26b099077b8152f6dab8f0c697e007085ee16031
Bogdan Petryakov <43905628+bogdyname@users.noreply.github.com>
2023-07-31 16:49:44 +0300

    added new script
    -- add new script to calculate compound interest
    -- refactored script to create sql file for load

---
+++ Python scripts/compound_interest.py
@@ -0,0 +1,20 @@
+
+def compound_interest(years=9, percent=10, start_capital=500000, capital_increase=500000) -> int:
+    
+    result=0
+    for year in range(years):
+        result = start_capital+(start_capital*(percent/100))
+        print('Sum for', year, 'year wihtout increase:', "{:,.2f}".format(result))
+        
+        result += capital_increase
+        start_capital = result
+        print('Sum for', year, 'year with increase:', "{:,.2f}".format(result))
+
+if __name__ == "__main__":
+    
+    years = 30 #start from zero
+    percent = 9 #%
+    start_capital = 500000 #500'000
+    capital_increase = 500000 #250'000
+    
+    compound_interest(years, percent, start_capital, capital_increase)
--- Python scripts/mysql_load_data.py
+++ Python scripts/mysql_load_data.py
@@ -1,51 +1,22 @@
-import sys
-from datetime import datetime
-
-def check_emptiness(args=[]) -> bool:
-    """
-    Function to check list of args for null and data type.
-    
-    Parameters
-    ----------
-    args : list
-        List of args to check
+import csv
+from datetime import datetime     
         
-    Returns
-    -------
-    true on success otherwise false
-    
-    Raises
-    ------
-    ValueError
-        If args is empty.
-        If any item in args is empty.
-        If any item in args is not a string.
-    """
-    if not args:
-        raise ValueError('args list is empty')
-    
-    #check data type
-    if any(not isinstance(item, str) for item in args):
-        raise ValueError('some value is not a string')
-    #check data in args
-    if any(not item for item in args):
-        raise ValueError('some value is empty')
-    
-    return True
-        
-def create_insert_command(table: str, field: dict, filename='insert') -> str:
+def create_insert_command(table: str, field: dict, csv_field: list, input_filename: str, output_filename: str) -> str:
     """
      Function to check list of args for null and data type.
     
     Parameters
     ----------
-    filename : str, optional
-        File name there to save created sql command to isert.
     table : str
         Table name there need to isert new data.
-    
     field : dict
-        (key-str, value-list)
+        Fields names in table. Key is name, value is type.
+    csv_field : list
+        List of columns names in loaded csv file for keys in field dict.
+    input_filename : str
+        Csv filename for parsing.
+    output_filename : str
+        Name for output sql file.
         
     Returns
     -------
@@ -57,18 +28,52 @@ def create_insert_command(table: str, field: dict, filename='insert') -> str:
     """
     #add current date and time to output file
     current_dt = datetime.now().strftime("%d%m%Y_%H%M%S")
-    filename += '_' + current_dt + '.sql'
+    output_filename += '_' + current_dt + '.sql'
     
-    args = [table, field]
-    check_emptiness(args)
+    print('Output: ', output_filename)
+    print('Input: ', input_filename)
     
-def read_column_csv(filename='', column=''):
-    pass
+    #format for isert command
+    #INSERT INTO table_name (column1, column2, column3, ...)
+    #VALUES (value1, value2, value3, ...), (value1, value2, value3, ...), etc;
+    
+    # reading CSV file
+    if input_filename:
+        # open the file in read mode
+        filename = open(input_filename, 'r')
+        # creating dictreader object
+        file = csv.DictReader(filename)
+
+        number_from = []
+        number_to = []
+        owner = []
+        mnc = []
+        region = []
+        tp = []
+        
+        for col in file:
+            number_from.append(col['NumberFrom'])
+            number_to.append(col['NumberTo'])
+            owner.append(col['OwnerId'])
+            mnc.append(col['MNC'])
+            region.append(col['RegionCode'])
+            tp.append(1)
+            
+        insert_com = 'INSERT INTO 7_zone (number_from, number_to, operator, mnc, region, type) \n VALUES \n'
+        
+        for n_from, n_to, own, m, reg, t in zip(number_from, number_to, owner, mnc, region, tp):
+            insert_com += '(' + n_from + '",' + '"' + n_to + '",' + '"' + own + '",' + str(m) + ',' + str(reg) + ',' + str(t) + ',' + '),\n'
+ 
+        text_file = open(output_filename, "w")
+        text_file.write(insert_com)
+        text_file.close()
     
 if __name__ == "__main__":
-    filename = ''
-    if len (sys.argv) == 2:
-        filename = sys.argv[1]
-    else:
-        print('Error: add path to filename!')
-        quit()
+    
+    table = '7_zone'
+    field = {'number_from': 'VARCHAR', 'number_to': 'VARCHAR', 'operator': 'VARCHAR', 'mnc': 'INT', 'region': 'INT', 'type': 'TINYINT'}
+    csv_field = ['NumberFrom', 'NumberTo', 'OwnerId', 'MNC', 'RegionCode', 'type']
+    input_filename = r'C:\\Users\\bogdy\\Downloads\\formated\\Numbering_plan_202307180000_3528.csv'
+    output_filename = r'C:\\Users\\bogdy\\Downloads\\formated\\7_zone.sql'
+
+    create_insert_command(table, field, csv_field, input_filename, output_filename)
